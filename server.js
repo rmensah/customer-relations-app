9535bb7be441d2e3aa3dd206cdea826cf97d157e
RM <rm@RMs-MacBook-Pro.fios-router.home>
2016-04-28 11:53:47 -0400

    set up app and routes in server.js

---
+++ server.js
@@ -0,0 +1,70 @@
+//BASE SETUP
+
+//===============================================
+
+//CALL THE PACKAGES
+
+var express = require('express'); // call express
+var app = express(); //define app using express
+var bodyParser = require('body-parser'); //get body-parser
+var morgan = require('morgan'); // used to see requests
+var mongoose = require('mongoose'); // for working w/our database
+
+var port = process.env.PORT || 8080; // set the port for the app
+
+
+// APP CONFIGURATION ------------------
+// Use body parser so we can grab information from post requests
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+//configure app to handle CORS requests
+
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin' , '*');
+
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers' , 'X-Requested-With,content-type, \Authorization');
+	next();
+
+});
+
+//log all requests to the console
+app.use(morgan('dev'));
+
+// ROUTES FOR THE API
+//====================================
+
+//basic route for the home page
+
+app.get('/', function(req, res) {
+	res.send('Welcome to the home page!');
+});
+
+//get an instance of the express router
+
+var apiRouter = express.Router();
+
+//test route to make sure everything is working
+//accessed at GET http://locolhost:8080/api
+
+apiRouter.get('/', function(req, res) {
+	res.json({ message: 'hooray! Welcome to our api!' });
+});
+
+//more routes for the API will go here
+
+
+//REGISTER ROUTES=====================
+
+// all routes will be prefixed with /api
+
+app.use('/api' , apiRouter);
+
+//START THE SERVER
+//======================
+
+app.listen(port);
+console.log('Magic happens on port ' + port);

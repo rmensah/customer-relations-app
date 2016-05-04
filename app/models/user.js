cfdb49b4cdb5e062a833c3e6733634b6933f48f6
rmensah <richiemens@gmail.com>
2016-05-04 08:08:47 -0400

    added password hash/ strategy

--- app/models/user.js
+++ app/models/user.js
@@ -19,3 +19,38 @@ var UserSchema = new Schema({
 			select: false}
 
 });
+
+//hash the password before the user is saved
+
+UserSchema.pre('save' , function(next) {
+	var user = this;
+
+// hash the password only if the password has been changed or user is new
+
+	if(!user.isModified('password')) return next();
+
+
+	//generate the hash
+	bcrypt.hash(user.password, null, null function(err, hash) {
+		if(err) return next(err);
+
+		//change the password to the hashed version
+
+			user.password = hash;
+			next();
+	});
+
+});
+
+// method to compare given password with the database hash
+
+UserSchema.methods.comparePassword = function(pass) {
+		var user = this;
+
+		return bcrypt.compareSync(password,user.password);
+
+};
+
+//return the model
+
+model.exports = mongoose.model('User' , UserSchema);
